66b19b0d23748c2ff5ca2a2a5b6664783e7bf1a2
engalar <engalar@gmail.com>
2024-12-12 14:21:53 +0800

    refacor

---
+++ src/batchGenAttribue.ts
@@ -0,0 +1,53 @@
+import { OnlineWorkingCopy } from "mendixplatformsdk";
+import { addEnity } from "./DomainModel";
+
+export async function batchGenAttribue(
+    workingCopy: OnlineWorkingCopy,
+    moduleName: string,
+    entityName: string,
+) {
+    const model = await workingCopy.openModel();
+
+    const domainModelInterface = model
+        .allDomainModels()
+        .filter((dm) => dm.containerAsModule.name === moduleName)[0];
+    const domainModel = await domainModelInterface.load();
+
+    const attributes = [
+        "OpportunityId",
+        "AccountName",
+        "AccountNameLocal",
+        "OpportunityName",
+        "PartnerAccount",
+        "Tier2PartnerAccount",
+        "PartnerRegistrationType",
+        "OpportunityRecordType",
+        "OwnerRole",
+        "OpportunityOwner",
+        "Stage",
+        "FiscalPeriod",
+        "Probability",
+        "Age",
+        "TotalLicenseMargin",
+        "TotalHwMaintenanceMargin",
+        "CloseDate",
+        "NextStep",
+        "GscsNamedAccount",
+        "LastActivity",
+        "TotalPerpetualCurrency",
+        "TotalPerpetual",
+        "TotalHardwareCurrency",
+        "TotalHardware",
+        "TotalLicenseCurrency",
+        "TotalLicense",
+        "TotalSubscriptionsCurrency",
+        "TotalSubscriptions",
+        "TotalOpportunityCurrency",
+        "TotalOpportunity",
+    ];
+
+    addEnity(domainModel, entityName, attributes);
+
+    await model.flushChanges();
+    await workingCopy.commitToRepository("main");
+}
---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import { MendixPlatformClient } from "mendixplatformsdk";
+import { batchGenMicroflow } from "./batchGenMicroflow";
+import { batchGenAttribue } from "./batchGenAttribue";
+
+//https://sprintr.home.mendix.com/link/teamserver/4160aabf-9ee0-44ce-aa1f-ec4cf6a4243c
+const APP_ID = "4160aabf-9ee0-44ce-aa1f-ec4cf6a4243c";
+// PAT 写入环境变量 MENDIX_TOKEN
+// https://user-settings.mendix.com/link/developersettings
+// Model Repository + Model Server
+
+async function main() {
+    const workingCopy = await getApp(APP_ID);
+
+    //MyFirstModule
+    const moduleName = "DiscountAutomation",
+        entityName = "Opportunity2",
+        microflowName = "Microflow";
+    // await batchGenAttribue(workingCopy, moduleName, entityName);
+    await batchGenMicroflow(
+        workingCopy,
+        `${moduleName}.${microflowName}`,
+        `${moduleName}.${entityName}`,
+    );
+
+    async function getApp(ai: string) {
+        const client = new MendixPlatformClient();
+        const app = await client.getApp(ai);
+
+        const workingCopy = await app.createTemporaryWorkingCopy("main");
+        return workingCopy;
+    }
+}
+
+main().catch(console.error);
