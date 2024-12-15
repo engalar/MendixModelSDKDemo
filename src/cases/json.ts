80ab6ababb8bb77f92a314f0dd012dcd33013fe6
engalar <engalar@gmail.com>
2024-12-15 16:28:07 +0800

    struct domain and mf

--- src/cases/json.ts
+++ src/cases/json.ts
@@ -3,8 +3,22 @@ import { boot } from "../lib/bootstrap";
 import { _saveToDumyFile } from "../lib/serialize";
 
 boot(async (model) => {
+    // page
     const iPage = model.findPageByQualifiedName("MyFirstModule.Home_Web");
     const page = await iPage.load();
     _saveToDumyFile(JSON.stringify(page.toJSON()), "page.structure.json");
+    // domain
+    const iModule = model.findModuleByQualifiedName("MyFirstModule");
+    const module = await iModule.domainModel.load();
+    _saveToDumyFile(JSON.stringify(module.toJSON()), "domain.structure.json");
+    // microflow
+    const iMicroflow = model.findMicroflowByQualifiedName(
+        "MyFirstModule.DS_GetUser",
+    );
+    const microflow = await iMicroflow.load();
+    _saveToDumyFile(
+        JSON.stringify(microflow.toJSON()),
+        "microflow.structure.json",
+    );
     return false;
 });
